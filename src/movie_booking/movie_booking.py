27b0256b0a9c7786725a3b96fbb57840e10fa4e4
babi <debisaabebesa@gmail.com>
2026-03-05 11:45:00 +0300

    initial group 9 project folder structor

---
+++ src/movie_booking/movie_booking.py
@@ -0,0 +1,6 @@
+# Task 4: Movie Ticket Booking Simulation
+# Simulate a movie theater booking system that:
+#  • Shows a list of available movie titles, showtimes, and seat prices.
+#  • Asks the user to choose a movie and number of tickets.
+#  • Confirms total price and asks if they want to book another movie.
+#  • Ends when they say "no" and displays total bookings and cost.
---
+++ src/quiz_game/quiz_game.py
@@ -0,0 +1,7 @@
+# Task 5: Quiz Game
+# Create a basic quiz game that:
+#  • Contains a list of 5–10 questions stored in a dictionary (or list of dictionaries [{}, {}] ).
+#  • Ask the user each question and records their answers.
+#  • At the end, displays:
+#    o The user's score (e.g., 7/10)
+#    o Correct answers for any questions they got wrong
